266817f049e56b5f85f303953f2260ffbd45f143
stefan-nikola <stefanovicnikola997@gmail.com>
2023-10-12 18:21:23 +0200

    Promenio stvari za Hakaton 9

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@
 /* ==========================================================================
    countdown timer
    ========================================================================== */
- jQuery('#clock').countdown('2022/11/04 15:00:00',function(event){
+ jQuery('#clock').countdown('2023/11/03 15:00:00',function(event){
   var $this=jQuery(this).html(event.strftime(''
   +'<div class="time-entry days"><span>%-D</span> Days</div> '
   +'<div class="time-entry hours"><span>%H</span> Hours</div> '
